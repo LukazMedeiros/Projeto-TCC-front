9a210221cfece2b1ab919d322c69be6e650fabc5
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2020-09-11 11:24:11 -0300

    criado esqueleto pagina de logon

--- src/pages/logon/index.js
+++ src/pages/logon/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './logon-style.css'
 
 export default function Logon() {
     return(
         <div className="logon">
-            pagina de logon
+        <div className="logo">
+            logotipo
+        </div>
+        <div className="formulario">
+            <form onSubmit="">
+                <input type="text" name="" id=""/>
+                <input type="password" name="" id=""/>
+                <button>Entrar</button>
+            </form>
+        </div>
         </div>
         );
     }
