e8461d0b9cd47fb5a37bb5d93b31571ae1d9f1d0
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2020-09-11 11:25:35 -0300

    criado estrutura basica pagina de menu interno

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './menu-style.css'
 
-export default function Menu(params) {
+export default function Menu() {
     return(
         <div className="menu">
-            menu
+            <header>
+                <div className="logo">logotipo</div>
+                <div className="opicoes">Bem-Vindo #Nome#</div>
+            </header>
+            <main>
+                <button>Novo</button>
+                <div className="itens">
+                    <ul>
+                        <li>incidentes</li>
+                    </ul>
+                </div>
+            </main>
         </div>
-    )
-}
+        )
+    }
